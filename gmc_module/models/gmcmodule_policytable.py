d9d938fec3e492d4801f7de03b2446bda90bd3cc
Sai <134498450+saimantha@users.noreply.github.com>
2023-06-09 12:59:04 +0100

    Update for error

--- gmc_module/models/gmcmodule_policytable.py
+++ gmc_module/models/gmcmodule_policytable.py
@@ -3,6 +3,6 @@ from odoo import models, fields
 
 
 class GMCModulePolicyTable(models.Model):
-	_name = 'GMCModule.PolicyTable'	
+	_name = 'gmcmodule.policytable'	
 	name = fields.Char('Title', required=True)	
 	
