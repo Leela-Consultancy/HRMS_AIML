1e55f12600fd3d1bc7cb3e3d352b4cd64377f212
Sai <134498450+saimantha@users.noreply.github.com>
2023-06-11 17:29:13 +0100

    Update gmcmodule_employeetable.py

--- gmc_module/models/gmcmodule_employeetable.py
+++ gmc_module/models/gmcmodule_employeetable.py
@@ -1,3 +1,4 @@
+from operator import index
 from odoo import models, fields
 
 
@@ -8,7 +9,7 @@ class GmcModuleEmployeeTable(models.Model):
 	cardno = fields.Char('Card No', required=True)
 	name = fields.Char('Title', required=True)	
 	dateofjoining = fields.Date('Date of joining', required=True)
-	policytype_id = fields.Char('Policy Type', required=True)
+	policytype_id = fields.Many2one('gmcmodule.policytable', ondelete='set null', string='Policy Type', index=True)
 	suminsured = fields.Float('Sum Insured(in Lac)', required=True)
 	proratapremium = fields.Float('Prorata Premium in Rs.', required=True)
 	coveringdays = fields.Integer('Covering Days', required=True)
