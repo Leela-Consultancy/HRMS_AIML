a462741382a48ae32e74e19db0cfeec19e1af71d
Sai <134498450+saimantha@users.noreply.github.com>
2023-06-23 18:23:52 +0100

    Distiction Computed fields

--- gmc_module/models/gmcmodule_employeetable.py
+++ gmc_module/models/gmcmodule_employeetable.py
@@ -8,8 +8,12 @@ class GmcModuleEmployeeTable(models.Model):
 	dateofjoining = fields.Date('Date of joining', required=True)
 	poltype_id = fields.Many2one('gmcmodule.policytable', ondelete='set null', string='Policy Type', index=True)
 	suminsured = fields.Float('Sum Insured(in Lac)', required=True)
-	proratapremium = fields.Float('Prorata Premium in Rs.', required=True)
+	proratapremium = fields.Float('Prorata Premium in Rs.', compute='_compute_premium', store=True)
 	coveringdays = fields.Integer('Covering Days', required=True)
 	strdate = fields.Date('Start Date', related='poltype_id.startdate', store=True)
 	enddate = fields.Date('End Date', related='poltype_id.enddate', store=True)
 
+	@api.depends('coveringdays', 'poltype_id', 'suminsured')
+	def _compute_premium(self):
+		for record in self:
+			record.proratapremium = ((record.poltype_id.premium)/365) * record.coveringdays * record.suminsured
